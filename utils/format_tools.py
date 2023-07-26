2b0d03356c30acbfd2f4ed9209d1372e469f642c
1375626371 <1375626371@qq.com>
2023-07-26 11:48:28 +0800

    finish chapter7 pre_trenslate
    9mdx 5mdx need to check

--- utils/format_tools.py
+++ utils/format_tools.py
@@ -2,10 +2,11 @@ import format_check
 import os
 
 def comfirm(file_name):
-    select=input("Warnding!!! 是否信任格式化后的版本，删除之前的版本?(y/n)")
-    if select=='y':
+    # select=input("Warnding!!! 是否信任格式化后的版本，删除之前的版本?(y/n)")
+    # if select=='y':
         os.remove(file_name)
         os.renames('.'.join(file_name.split('.')[:-1]+['_fix']+[file_name.split('.')[-1]]),file_name)
-file_name='Course\zh-CN\chapter6\\10.mdx'
-format_check.check_file(file_name,auto_fix=True)
-comfirm(file_name)
+for i in range(1,9):
+    file_name=f'Course\zh-CN\chapter7\\{i}.mdx'
+    format_check.check_file(file_name,auto_fix=True)
+    comfirm(file_name)
