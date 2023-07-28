926ade67ccfba1b1b54b0c98f78ee6abd935f458
1375626371 <1375626371@qq.com>
2023-07-28 16:43:41 +0800

    第一章 第二章 publish finish

--- utils/python_source_format.py
+++ utils/python_source_format.py
@@ -8,7 +8,8 @@ for mdx_file in os.listdir(fix_path):
         md_text =re.findall(r'```.*?(.*?)```',  text, flags=re.S)
         text=re.compile(r"```.*?(.*?)```",flags=re.I|re.S).sub('＋－｜－－ｎ－－｜－＋',text)
         for code in md_text:
-            text=text.replace('＋－｜－－ｎ－－｜－＋','\n'.join(["python"]+code.split('\n')),1)
+            text=text.replace('＋－｜－－ｎ－－｜－＋','```'+'\n'.join(["python"]+code.split('\n')[1:])+'```',1)
             # ignore_dict[f'＋－｜－－{len(ignore_dict)}－－｜－＋']=f'```{strQ2B(code)}```'
+        text.replace('\n\n\n','\n\n')
         with open(fix_path+mdx_file,'w',encoding='utf-8') as f:
             f.write(text)
