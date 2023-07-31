d6a1e4e1aba4f3dfde8dec00640e8b9f38dee10c
露凝 王 <Lunyn2020@outlook.com>
2023-08-01 01:04:51 +0800

    Revert "Merge remote-tracking branch 'upstream/main'"
    This reverts commit 29e3abfccf21aa267bdf9051972795af29e7e3e4.

--- utils/glossary_hint.py
+++ utils/glossary_hint.py
@@ -62,5 +62,5 @@ def main(file_name):
                 print(f'line {line_count}, Col {col}: {string}:{trans_dict[string]} ')
         line_count += 1
 if __name__ == '__main__':
-    main(r'Course\en\chapter8\1.mdx')
+    main(r'Course\en\chapter4\6.mdx')
 
