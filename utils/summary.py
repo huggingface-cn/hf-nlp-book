1f877f2bb7f85a49dd521957e18b9f787acb7ee1
1375626371 <1375626371@qq.com>
2023-08-20 09:48:40 +0800

    添加了第二版初稿

--- utils/summary.py
+++ utils/summary.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 mdx_file='Course/publish/all/all.mdx'
 res=re.findall('```.*?```',open(mdx_file,'r',encoding='utf-8').read(),re.S)
-res=[[len(i),i] for i in res ]
+res=[[i.count('\n'),i] for i in res ]
 res.sort(key=lambda x:x[0],reverse=True)
 df=pd.DataFrame(res,columns=['length','code'])
 df.to_excel('code.xlsx',index=False)
